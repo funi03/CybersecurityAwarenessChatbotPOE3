11c931089b56d67d56de092ce6ff1bcdc43f0195
funi03 <st10480209@rcconnect.edu.za>
2026-05-28 19:24:27 +0200

    i added more sentiment responses

--- sentiment_detect.cs
+++ sentiment_detect.cs
@@ -1,86 +1,122 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Cybersecurity_Awareness_Chatbot_Part2
 {
-    public class sentiment_detect
-    {
-     
-        // METHOD TO DETECT SENTIMENT
-        public string detect_sentiment(string message)
+    
+        public class sentiment_detect
         {
-            // LOWERCASE MESSAGE
-            message = message.ToLower();
+            // RANDOM OBJECT
+            Random random = new Random();
 
-            // WORRIED
-            if (message.Contains("worried") ||
-                message.Contains("scared") ||
-                message.Contains("afraid"))
+            // SENTIMENT DETECTION
+            public string detect_sentiment(string message)
             {
-                return "worried";
+                // LOWERCASE MESSAGE
+                message = message.ToLower();
+
+                // WORRIED
+                if (message.Contains("worried") ||
+                    message.Contains("scared") ||
+                    message.Contains("afraid"))
+                {
+                    return "worried";
+                }
+
+                // FRUSTRATED
+                else if (message.Contains("frustrated") ||
+                         message.Contains("angry") ||
+                         message.Contains("annoyed"))
+                {
+                    return "frustrated";
+                }
+
+                // CURIOUS
+                else if (message.Contains("curious") ||
+                         message.Contains("interested") ||
+                         message.Contains("learn"))
+                {
+                    return "curious";
+                }
+
+                // HAPPY
+                else if (message.Contains("happy") ||
+                         message.Contains("good") ||
+                         message.Contains("great"))
+                {
+                    return "happy";
+                }
+
+                // SAD
+                else if (message.Contains("sad") ||
+                         message.Contains("upset") ||
+                         message.Contains("depressed"))
+                {
+                    return "sad";
+                }
+
+                // NO SENTIMENT
+                return "neutral";
             }
 
-            // FRUSTRATED
-            else if (message.Contains("frustrated") ||
-                     message.Contains("angry") ||
-                     message.Contains("annoyed"))
+            // DYNAMIC SENTIMENT RESPONSES
+            public string sentiment_response(string feeling)
             {
-                return "frustrated";
+                List<string> responses = new List<string>();
+
+                // WORRIED
+                if (feeling == "worried")
+                {
+                    responses.Add("It is understandable to feel worried. Cyber threats are common, but I can help you stay safe online.");
+                    responses.Add("Do not panic. I can give you cybersecurity tips to stay protected.");
+                    responses.Add("Many people worry about online safety. Learning cybersecurity helps reduce risks.");
+                }
+
+                // FRUSTRATED
+                else if (feeling == "frustrated")
+                {
+                    responses.Add("I understand your frustration. Cybersecurity can feel overwhelming sometimes.");
+                    responses.Add("It is okay to feel frustrated. I will help you step by step.");
+                    responses.Add("Do not worry, cybersecurity becomes easier with practice.");
+                }
+
+                // CURIOUS
+                else if (feeling == "curious")
+                {
+                    responses.Add("That is great! Learning about cybersecurity helps you stay protected online.");
+                    responses.Add("Curiosity is important in cybersecurity awareness.");
+                    responses.Add("The more you learn about cybersecurity, the safer you become online.");
+                }
+
+                // HAPPY
+                else if (feeling == "happy")
+                {
+                    responses.Add("I am glad you are feeling positive about cybersecurity awareness!");
+                    responses.Add("That is wonderful to hear!");
+                    responses.Add("Positivity makes learning easier and more enjoyable.");
+                }
+
+                // SAD
+                else if (feeling == "sad")
+                {
+                    responses.Add("I am sorry you are feeling sad. I am here to help.");
+                    responses.Add("Take things one step at a time. You are doing well.");
+                    responses.Add("Cybersecurity can be stressful, but learning helps build confidence.");
+                }
+
+                // RETURN RANDOM RESPONSE
+                if (responses.Count > 0)
+                {
+                    int index = random.Next(responses.Count);
+
+                    return responses[index];
+                }
+
+                // DEFAULT
+                return "";
             }
 
-            // CURIOUS
-            else if (message.Contains("curious") ||
-                     message.Contains("interested") ||
-                     message.Contains("learn"))
-            {
-                return "curious";
-            }
+        }//end class
+    }//end namespace
 
-            // HAPPY
-            else if (message.Contains("happy") ||
-                     message.Contains("good") ||
-                     message.Contains("great"))
-            {
-                return "happy";
-            }
-
-            // NO SENTIMENT FOUND
-            return "neutral";
-        }
-
-        // METHOD TO RETURN RESPONSE
-        public string sentiment_response(string feeling)
-        {
-
-            // WORRIED RESPONSE
-            if (feeling == "worried")
-            {
-                return "It is understandable to feel worried. Cyber threats are common, but I can help you stay safe online.";
-
-            }
-
-            // FRUSTRATED RESPONSE
-            else if (feeling == "frustrated")
-            {
-                return "I understand your frustration. Cybersecurity can feel overwhelming, but small safety steps make a big difference.";
-            }
-
-            // CURIOUS RESPONSE
-            else if (feeling == "curious")
-            {
-                return "That is great! Learning about cybersecurity helps you stay protected online.";
-            }
-
-            // HAPPY RESPONSE
-            else if (feeling == "happy")
-            {
-                return "I am glad you are feeling positive about cybersecurity awareness!";
-            }
-
-            // DEFAULT RESPONSE
-            return "";
-        }
-
-
-
-    }
-}
